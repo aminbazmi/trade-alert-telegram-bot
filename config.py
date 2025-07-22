51fc9ba5b0b32ad2fe9fff98bbf59dbff24577e7
aminbazmi <amin_bazmiii@yahoo.com>
2025-07-23 01:18:04 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+# اطلاعات پیکربندی بات
+
+TELEGRAM_BOT_TOKEN = "7851579118:AAH-Is1Q5RbtQp-H3_jY9bEprTaOpOctQ4A"
+TELEGRAM_CHAT_ID = "@aminbazmi"
+
+# لیست ارزهایی که می‌خوای زیر نظر بگیری
+WATCHLIST = [
+    "FLOKI",
+    "BTC",
+    "ETH",
+    "MEW",
+    "SOL",     # سولانا
+    "DOGE",    # دوج‌کوین
+    "ADA",     # کاردانو
+    "PEPE",    # پپه
+    "XRP"      # ریپل
+]
